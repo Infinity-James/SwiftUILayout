edfbc26aa6d54512e192a3979cf2d1cd49aaaed8
James Valaitis <james@infinityjames.com>
2020-10-21 11:25:13 +0100

    Initial Commit

---
+++ SwiftUILayout/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUILayout
+//
+//  Created by James Valaitis on 21/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
